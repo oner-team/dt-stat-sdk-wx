0fff04a7d63a59b0f17c23c1d22a802d77a5ba86
刘鑫 <liu_narcisus@163.com>
2018-11-12 11:11:13 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,18 @@
+var conf = {
+  name: 'stat',
+  appid: 'wx16ce2f6e06acd4d5',
+  server_url:'https://log.dtwave-inc.com/sa.gif?project=xubo&token=27f1e21b78daf376',
+  send_timeout: 1000,
+  max_string_length: 300,
+  use_client_time: false,
+  show_log: true,
+  allow_amend_share_path: true,
+  autoTrack:{  
+    appLaunch: true, 
+    appShow: true, 
+    appHide: true, 
+    pageShow: true,
+  }
+};
+
+module.exports = conf;
